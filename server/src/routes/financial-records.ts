523125964a5544fbac06f52f02885e17cffd96f2
Kristin Diep <kristiyip@gmail.com>
2024-10-08 15:31:44 -0400

    create delete, add, update api

--- server/src/routes/financial-records.ts
+++ server/src/routes/financial-records.ts
@@ -3,6 +3,7 @@ import FinancialRecordModel from '../schema/financial-record';
 
 const router = express.Router();
 
+// @ts-ignore
 router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
   try{
     const userId = req.params.userId;
@@ -12,6 +13,52 @@ router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
     }
     res.status(200).send(records);
   } catch(err) {
+    res.status(500).send(err);
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try{
+    const newRecordBody = req.body;
+    const newRecord = new FinancialRecordModel(newRecordBody)
+    const savedRecord = await newRecord.save()
+
+
+    res.status(200).send(savedRecord);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+});
+
+// @ts-ignore
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const newRecordBody = req.body;
+    const record = await FinancialRecordModel.findByIdAndUpdate(
+      id, 
+      newRecordBody,
+      { new: true }
+    );
+
+    if(!record) return res.status(404).send();
+
+    res.status(200).send(record)
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
+// @ts-ignore
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const record = await FinancialRecordModel.findByIdAndDelete(id);
+
+    if(!record) return res.status(404).send();
+
+    res.status(200).send(record)
+  } catch (err) {
     res.status(500).send(err);
   }
 })
